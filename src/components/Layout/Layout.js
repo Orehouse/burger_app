4c9a14d484bd78493e802ffaaad85aa76bac1a4e
Орехов Артем <orehouse@ya.ru>
2019-08-12 22:48:20 +0300

    Impoving performance

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -7,21 +7,23 @@ import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 class Layout extends Component {
   state = {
-    showSideDrawer: true
+    showSideDrawer: false
   };
 
   sideDrawerClosedHandler = () => {
     this.setState({ showSideDrawer: false });
   };
 
-  sideDrawerOpenHandler = () => {
-    this.setState({ showSideDrawer: true });
+  sideDrawerToggleHandler = () => {
+    this.setState(prevState => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
   };
 
   render() {
     return (
       <Aux>
-        <Toolbar opened={this.sideDrawerOpenHandler} />
+        <Toolbar sideDrawerToggleClicked={this.sideDrawerToggleHandler} />
         <SideDrawer
           closed={this.sideDrawerClosedHandler}
           open={this.state.showSideDrawer}

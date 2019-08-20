cf37ef1a7b59eedbc204bdae0723188fb96ac3d3
Орехов Артем <orehouse@ya.ru>
2019-08-20 17:31:58 +0300

    Revert "unused actions removed"
    This reverts commit 6473367d88ccc9a610a2c33ad3e7f82f787a271e.

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -2,12 +2,17 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 
 import axios from "../../axios-orders";
+import * as actions from "../../store/actions/index";
 
 import Order from "./Order/Order";
 import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 import Spinner from "../../components/UI/Spinner/Spinner";
 
 class Orders extends Component {
+  componentDidMount() {
+    this.props.onFetchOrders();
+  }
+
   render() {
     const orders =
       this.props.orders.length > 0 ? (
@@ -22,10 +27,18 @@ class Orders extends Component {
 }
 
 const mapStateToProps = state => {
-  return {
-    orders: state.order.orders,
-    loading: state.order.loading
+    return {
+      orders: state.order.orders,
+      loading: state.order.loading
+    };
+  },
+  mapDispatchToProps = dispatch => {
+    return {
+      onFetchOrders: () => dispatch(actions.fetchOrders())
+    };
   };
-};
 
-export default connect(mapStateToProps)(withErrorHandler(Orders, axios));
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(withErrorHandler(Orders, axios));

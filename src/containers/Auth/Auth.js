cb366cbd2e65f7a1a3cdd8fc88bd0a2dfae1bc96
Орехов Артем <orehouse@ya.ru>
2019-08-20 17:15:40 +0300

    Auth initial container added

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,106 @@
+import React, { Component } from "react";
+
+import Input from "../../components/UI/Input/Input";
+import Button from "../../components/UI/Button/Button";
+
+import classes from "./Auth.module.css";
+
+class Auth extends Component {
+  state = {
+    controls: {
+      email: {
+        elementType: "input",
+        elementConfig: { type: "email", placeholder: "e-mail address" },
+        value: "",
+        validation: {
+          required: true,
+          isEmail: true
+        },
+        valid: false,
+        touched: false
+      },
+      password: {
+        elementType: "input",
+        elementConfig: { type: "password", placeholder: "Password" },
+        value: "",
+        validation: {
+          required: true,
+          minLength: 6
+        },
+        valid: false,
+        touched: false
+      }
+    }
+  };
+
+  checkValidity = (value, rules) => {
+    let isValid = false;
+
+    if (rules.required) {
+      isValid = value.trim() !== "";
+    }
+
+    if (rules.minLength) {
+      isValid &= value.trim().length >= rules.minLength;
+    }
+
+    if (rules.maxLength) {
+      isValid &= value.trim().length <= rules.maxLength;
+    }
+
+    if (rules.isEmail) {
+      isValid &= new RegExp(
+        /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+      ).test(value);
+    }
+
+    return !!isValid;
+  };
+
+  inputChangedHandler = (event, controlName) => {
+    const updatedControls = {
+      ...this.state.controls,
+      [controlName]: {
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        valid: this.checkValidity(
+          event.target.value,
+          this.state.controls[controlName].validation
+        ),
+        touched: true
+      }
+    };
+    this.setState({ controls: updatedControls });
+  };
+
+  render() {
+    const formElements = [];
+    for (let key in this.state.controls) {
+      formElements.push({
+        id: key,
+        config: this.state.controls[key]
+      });
+    }
+    const form = formElements.map(formElement => (
+      <Input
+        key={formElement.id}
+        elementType={formElement.config.elementType}
+        elementConfig={formElement.config.elementConfig}
+        value={formElement.config.value}
+        validated={formElement.config.valid}
+        touched={formElement.config.touched}
+        changed={event => this.inputChangedHandler(event, formElement.id)}
+      />
+    ));
+    return (
+      <div className={classes.Auth}>
+        <form>
+          {form}
+          <Button btnType={"Success"}>SUBMIT</Button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Auth;
